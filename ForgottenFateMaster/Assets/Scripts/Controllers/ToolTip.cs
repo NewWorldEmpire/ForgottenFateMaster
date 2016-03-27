5aedce62cdcf4c20e1b3d53afa5844f719c177be
Sam <gebharsj@gmatc.matc.edu>
2016-03-27 00:19:33 -0500

    fixed character stats scaling issue

--- ForgottenFateMaster/Assets/Scripts/Controllers/ToolTip.cs
+++ ForgottenFateMaster/Assets/Scripts/Controllers/ToolTip.cs
@@ -25,8 +25,17 @@ public class ToolTip : MonoBehaviour
     public GameObject _criticalChance;
     int shuffle = 0;
 
+    Vector3 spellScale;
+    Vector3 spellScaledUp;
+    Vector3 damageScale;
+    Vector3 damageScaledUp;
+
     void Start()
     {
+        spellScale = new Vector3(_spell.transform.localScale.x, _spell.transform.localScale.y, 0f);
+        spellScaledUp = new Vector3(_spell.transform.localScale.x + 0.10f, _spell.transform.localScale.y + 0.10f, 0f);
+        damageScale = new Vector3(_damage.transform.localScale.x, _damage.transform.localScale.y, 0f);
+        damageScaledUp = new Vector3(_damage.transform.localScale.x + 0.10f, _damage.transform.localScale.y + 0.10f, 0f);
         au_click = gameObject.AddComponent<AudioSource>();
         AudioClip click;
 
@@ -47,19 +56,68 @@ public class ToolTip : MonoBehaviour
         }
     }
 
-    public void TipOff()
+    public void ArmorTipOff()
     {
         //if (_ToolTip.active)
         _ToolTip.SetActive(false);
         au_click.Play();
-        _defense.transform.localScale = new Vector3(0.3f, 0.3f, 0f);
-        _dexterity.transform.localScale = new Vector3(0.3f, 0.3f, 0f);
-        _damage.transform.localScale = new Vector3(0.3f, 0.3f, 0f);
-        _armor.transform.localScale = new Vector3(0.3f, 0.3f, 0f);
-        _spell.transform.localScale = new Vector3(0.3f, 0.3f, 0f);
-        _stamina.transform.localScale = new Vector3(0.3f, 0.3f, 0f);
-        _health.transform.localScale = new Vector3(0.3f, 0.3f, 0f);
-        _criticalChance.transform.localScale = new Vector3(0.3f, 0.3f, 0f);
+        _armor.transform.localScale = new Vector3(_armor.transform.localScale.x - 0.10f, _armor.transform.localScale.y - 0.10f, 0f);
+        shuffle = 0;
+    }
+
+    public void LuckTipOff()
+    {
+        _ToolTip.SetActive(false);
+        au_click.Play();
+        _criticalChance.transform.localScale = new Vector3(_criticalChance.transform.localScale.x - 0.10f, _criticalChance.transform.localScale.y - 0.10f, 0f);
+        shuffle = 0;
+    }
+
+    public void HealthTipOff()
+    {
+        _ToolTip.SetActive(false);
+        au_click.Play();
+        _health.transform.localScale = new Vector3(_health.transform.localScale.x - 0.10f, _health.transform.localScale.y - 0.10f, 0f);
+        shuffle = 0;
+    }
+
+    public void StaminaTipOff()
+    {
+        _ToolTip.SetActive(false);
+        au_click.Play();
+        _stamina.transform.localScale = new Vector3(_stamina.transform.localScale.x - 0.10f, _stamina.transform.localScale.y - 0.10f, 0f);
+        shuffle = 0;
+    }
+
+    public void DefenseTipOff()
+    {
+        _ToolTip.SetActive(false);
+        au_click.Play();
+        _defense.transform.localScale = new Vector3(_defense.transform.localScale.x - 0.10f, _defense.transform.localScale.y - 0.10f, 0f);
+        shuffle = 0;
+    }
+
+    public void DamageTipOff()
+    {
+        _ToolTip.SetActive(false);
+        au_click.Play();
+        _damage.transform.localScale = damageScale;
+        shuffle = 0;
+    }
+
+    public void SpellTipOff()
+    {
+        _ToolTip.SetActive(false);
+        au_click.Play();
+        _spell.transform.localScale = spellScale;
+        shuffle = 0;
+    }
+
+    public void DexterityTipOff()
+    {
+        _ToolTip.SetActive(false);
+        au_click.Play();
+        _dexterity.transform.localScale = new Vector3(_dexterity.transform.localScale.x - 0.10f, _dexterity.transform.localScale.y - 0.10f, 0f);
         shuffle = 0;
     }
 
@@ -70,7 +128,7 @@ public class ToolTip : MonoBehaviour
         _ToolTipName.text = "Armor";
         _ToolTipDesc.text = "The amount of damage that is reduced.";
         _ToolTipAttri.text = "Armor: " + _player.GetComponent<CombatScript>().armor;
-        _armor.transform.localScale = new Vector3(0.4f, 0.4f, 0f);
+        _armor.transform.localScale = new Vector3(_armor.transform.localScale.x + 0.10f, _armor.transform.localScale.y + 0.10f, 0f);
     }
 
     public void LuckTipOn()
@@ -80,7 +138,7 @@ public class ToolTip : MonoBehaviour
         _ToolTipName.text = "Luck";
         _ToolTipDesc.text = "The chances of landing a critical hit and the amount of damage caused by critical hit.";
         _ToolTipAttri.text = "Critical Chance:      " + _player.GetComponent<CombatScript>().criticalChance * 100 + "%" + " \tCritical Damage: " + _player.GetComponent<CombatScript>().criticalDamage + "00%";
-        _criticalChance.transform.localScale = new Vector3(0.4f, 0.4f, 0f);
+        _criticalChance.transform.localScale = new Vector3(_criticalChance.transform.localScale.x + 0.10f, _criticalChance.transform.localScale.y + 0.10f, 0f);
     }
 
     public void HealthTipOn()
@@ -90,7 +148,7 @@ public class ToolTip : MonoBehaviour
         _ToolTipName.text = "Vitality";
         _ToolTipDesc.text = "The maximum amount of health.";
         _ToolTipAttri.text = "Health: " + _player.GetComponent<CombatScript>().maxHealth;
-        _health.transform.localScale = new Vector3(0.4f, 0.4f, 0f);
+        _health.transform.localScale = new Vector3(_health.transform.localScale.x + 0.10f, _health.transform.localScale.y + 0.10f, 0f);
     }
     public void StaminaTipOn()
     {
@@ -99,7 +157,7 @@ public class ToolTip : MonoBehaviour
         _ToolTipName.text = "Agility";
         _ToolTipDesc.text = "The maximum amount of stamina and the speed at which is travled.";
         _ToolTipAttri.text = "Stamina: " + _player.GetComponent<PlayerMovement>().maxStamina + "              \tSpeed:    " + _player.GetComponent<PlayerMovement>().speed + " - " + _player.GetComponent<PlayerMovement>().speed * _player.GetComponent<PlayerMovement>().sprint;
-        _stamina.transform.localScale = new Vector3(0.4f, 0.4f, 0f); ;
+        _stamina.transform.localScale = new Vector3(_stamina.transform.localScale.x + 0.10f, _stamina.transform.localScale.y + 0.10f, 0f); ;
     }
 
     public void DefenseTipOn()
@@ -109,7 +167,7 @@ public class ToolTip : MonoBehaviour
         _ToolTipName.text = "Defense";
         _ToolTipDesc.text = "Decreases the chances by which you are hit.";
         _ToolTipAttri.text = "Defense: " + _player.GetComponent<CombatScript>().defense;
-        _defense.transform.localScale = new Vector3(0.4f, 0.4f, 0f);
+        _defense.transform.localScale = new Vector3(_defense.transform.localScale.x + 0.10f, _defense.transform.localScale.y + 0.10f, 0f);
     }
 
     public void DamageTipOn()
@@ -119,7 +177,7 @@ public class ToolTip : MonoBehaviour
         au_click.Play();
         _ToolTipName.text = "Damage";
         _ToolTipDesc.text = "The minimum to maximum amount of damage dealt to enemies.";
-        _damage.transform.localScale = new Vector3(0.4f, 0.4f, 0f);
+        _damage.transform.localScale = damageScaledUp;
         shuffle = 1;
 
     }
@@ -150,7 +208,7 @@ public class ToolTip : MonoBehaviour
             _ToolTipAttri.text = "Absorbs Damage: +" + _player.GetComponent<CombatScript>().shield;
         if (_player.GetComponent<CombatScript>().spells == 3)
             _ToolTipAttri.text = "Shock Damage: " + _player.GetComponent<CombatScript>().lightDamage;
-        _spell.transform.localScale = new Vector3(0.4f, 0.4f, 0f);
+        _spell.transform.localScale = spellScaledUp;
     }
 
     public void DexterityTipOn()
@@ -160,6 +218,6 @@ public class ToolTip : MonoBehaviour
         _ToolTipName.text = "Dexterity";
         _ToolTipDesc.text = "Increases the chances of hitting your foes.";
         _ToolTipAttri.text = "Dexterity: " + _player.GetComponent<CombatScript>().dexterity;
-        _dexterity.transform.localScale = new Vector3(0.4f, 0.4f, 0f);
+        _dexterity.transform.localScale = new Vector3(_dexterity.transform.localScale.x + 0.10f, _dexterity.transform.localScale.y + 0.10f, 0f);
     }
 }
