559cfb2857a0ba1257527cc94fad0a29bf7cd512
gebharsj <gebharsj@gmatc.matc.edu>
2016-04-05 16:28:48 -0500

    edited control initializer

--- ForgottenFateMaster/Assets/Scripts/Menu/ControlInitialization.cs
+++ ForgottenFateMaster/Assets/Scripts/Menu/ControlInitialization.cs
@@ -5,43 +5,43 @@ public class ControlInitialization : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        if(PlayerPrefs.GetString("Interact") == " ")
+        if(PlayerPrefs.GetString("Interact") == "")
         {
             PlayerPrefs.SetString("Interact", "e");
         }
-        if (PlayerPrefs.GetString("MoveUp") == " ")
+        if (PlayerPrefs.GetString("MoveUp") == "")
         {
             PlayerPrefs.SetString("MoveUp", "w");
         }
-        if (PlayerPrefs.GetString("MoveLeft") == " ")
+        if (PlayerPrefs.GetString("MoveLeft") == "")
         {
             PlayerPrefs.SetString("MoveLeft", "a");
         }
-        if (PlayerPrefs.GetString("MoveDown") == " ")
+        if (PlayerPrefs.GetString("MoveDown") == "")
         {
             PlayerPrefs.SetString("MoveDown", "s");
         }
-        if (PlayerPrefs.GetString("MoveRight") == " ")
+        if (PlayerPrefs.GetString("MoveRight") == "")
         {
             PlayerPrefs.SetString("MoveRight", "d");
         }
-        if (PlayerPrefs.GetString("SwitchWeap") == " ")
+        if (PlayerPrefs.GetString("SwitchWeap") == "")
         {
             PlayerPrefs.SetString("SwitchWeap", "q");
         }
-        if (PlayerPrefs.GetString("Spell1") == " ")
+        if (PlayerPrefs.GetString("Spell1") == "")
         {
             PlayerPrefs.SetString("Spell1", "1");
         }
-        if (PlayerPrefs.GetString("Spell2") == " ")
+        if (PlayerPrefs.GetString("Spell2") == "")
         {
             PlayerPrefs.SetString("Spell2", "2");
         }
-        if (PlayerPrefs.GetString("Spell3") == " ")
+        if (PlayerPrefs.GetString("Spell3") == "")
         {
             PlayerPrefs.SetString("Spell3", "3");
         }
-        if (PlayerPrefs.GetString("Spell4") == " ")
+        if (PlayerPrefs.GetString("Spell4") == "")
         {
             PlayerPrefs.SetString("Spell4", "4");
         }
